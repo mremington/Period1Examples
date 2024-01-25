77dc72516e8ae17ff78469d58af3d8e41f3ef45e
mremington <michael.remington@sduhsd.net>
2024-01-25 17:09:57 +0000

    v1

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main{
   public static void main(String[] args){
-    System.out.println("Hello Ravens!");
-    
+    System.out.println("Hello Ravens! Lets write some practice code.");
+    //test the quadrant static method
+    System.out.println("Quadrant for point -4.2, 17.3 is: "+MyStaticMethods.quadrant(-4.2, 17.3));
   }
 }
